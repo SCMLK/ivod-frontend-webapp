db0d2f98870eb144ef84db814f4217de28ff5a6e
m-rothe <marcel@henkerservices.de>
2021-03-18 13:02:55 +0100

    Implemented other API calls

--- src/Upload.js
+++ src/Upload.js
@@ -5,7 +5,7 @@ export default function Upload() {
 
     const onChangeHandler = (event) => {
         //Do Upload here
-        getClient().uploadData(event.target.files[0], 'HARDCODED_NAME', console.log, errorHandler);
+        getClient().createDatasourceFromFile(event.target.files[0], 'HARDCODED_NAME', console.log, errorHandler);
     }
 
     const errorHandler = (error) => {
--- src/callWrapper.js
+++ src/callWrapper.js
@@ -2,15 +2,12 @@ import axios from "axios";
 
 var currentClient = [];
 
-//FIXME: Login Management
 class Client {
     constructor(baseURL) {
-        this.baseURL = baseURL; //FIXME: Sanitize input/remove trailing slash
         this.client = axios.create({
             baseURL: baseURL,
             timeout: 5000,
         });
-        //this.user = null;
         this.accessToken = null;
         this.refreshToken = null;
     }
@@ -18,7 +15,7 @@ class Client {
     login(username, password) {
         const param = {username: username, password: password};
         const client = this;
-        this.client({method: "POST", url:`/api/token/`, data: param, headers:{"Content-Type": "application/json"}})
+        return this.client({method: "POST", url:`/api/token/`, data: param, headers:{"Content-Type": "application/json"}})
             .then((response) => {
                 console.log(response)
                 client.accessToken = response.data['access'];
@@ -27,10 +24,13 @@ class Client {
             }).catch(console.error);
     }
 
-    uploadData(file, name, onSuccess, onError) {
-        if(!(this.accessToken)) {
-            throw "Not logged in!"
-        }
+    logout() {
+        this.accessToken = null;
+        this.refreshToken = null;
+    }
+
+    async createDatasourceFromFile(file, name, onSuccess, onError) {
+        await this.assertLogin()
         toBase64(file, (event) => {
             const param = {
                 datasource_name: name,
@@ -41,7 +41,59 @@ class Client {
         }, onError);
     }
 
-    getCharts(chartType, createdBefore, createdAfter, modifiedBefore, modifiedAfter, onSuccess, onError) {
+    async createDatasourceFromURL(url, name) {
+        await this.assertLogin()
+        const param = {
+            datasource_name: name,
+            url: url,
+        };
+        const response = await this.client({method: "POST", url:`/api/datasources`, data: param, headers:{"Content-Type": "application/json","Authorization":`Bearer ${this.accessToken}`}})
+        return response.data;
+    }
+
+    async getDatasources() {
+        const config = {method: "GET", url:`/api/datasources`}
+        if(this.accessToken) {
+            config['headers'] = {"Content-Type": "application/json","Authorization":`Bearer ${this.accessToken}`}
+        }
+        const response = await this.client(config);
+        return response.data;
+    }
+
+    async getDatasource(datasourceID) {
+        const config = {method: "GET", url:`/api/datasource/${datasourceID}`}
+        if(this.accessToken) {
+            config['headers'] = {"Content-Type": "application/json","Authorization":`Bearer ${this.accessToken}`}
+        }
+        const response = await this.client(config);
+        return response.data;
+    }
+
+    async updateDatasource(datasourceID, name) {
+        await this.assertLogin();
+        const param = {
+            datasource_name: name,
+        };
+        const response = await this.client({method: "PATCH", url:`/api/datasources/${datasourceID}`, data: param, headers:{"Content-Type": "application/json","Authorization":`Bearer ${this.accessToken}`}})
+        return response.data;
+    }
+
+    async deleteDatasource(datasourceID) {
+        await this.assertLogin();
+        const response = await this.client({method: "PATCH", url:`/api/datasources/${datasourceID}`, headers:{"Content-Type": "application/json","Authorization":`Bearer ${this.accessToken}`}})
+        return response.data;
+    }
+
+    async getChartTypes(datasourceID) {
+        const config = {method: "GET", url:`/api/datasources/${datasourceID}/charttypes`}
+        if(this.accessToken) {
+            config['headers'] = {"Content-Type": "application/json","Authorization":`Bearer ${this.accessToken}`}
+        }
+        const response = await this.client(config);
+        return response.data;
+    }
+
+    async getCharts(chartType, createdBefore, createdAfter, modifiedBefore, modifiedAfter) {
         const param =  {}
         if(chartType) {
             param['chart_type'] = chartType;
@@ -63,7 +115,162 @@ class Client {
             config['headers'] = {"Content-Type": "application/json","Authorization":`Bearer ${this.accessToken}`}
         }
 
-        this.client(config).then(onSuccess).catch(onError);
+        const response = await this.client(config);
+        return response.data;
+    }
+
+    async createChart(datasourceID, chartType, chartConfig, name, downloadable=false, visibility=0) {
+        await this.assertLogin();
+        const data = {datasource:datasourceID,chart_type:chartType, config:chartConfig, downloadable:downloadable, visibility:visibility}
+        const config = {method: "POST", url:`/api/charts`, data: data}
+        config['headers'] = {"Content-Type": "application/json","Authorization":`Bearer ${this.accessToken}`}
+        const response = await this.client(config);
+        return response.data;
+    }
+
+    async charttypes(chartID) {
+        const config = {method: "GET", url:`/api/charts/${chartID}`}
+        if(this.accessToken) {
+            config['headers'] = {"Content-Type": "application/json","Authorization":`Bearer ${this.accessToken}`}
+        }
+        const response = await this.client(config);
+        return response.data;
+    }
+
+    async modifyChart(chartID, modifications= {}) {
+        await this.assertLogin();
+        const config = {method: "PATCH", url:`/api/charts/${chartID}`, data: modifications}
+        config['headers'] = {"Content-Type": "application/json","Authorization":`Bearer ${this.accessToken}`}
+        const response = await this.client(config);
+        return response.data;
+    }
+
+    async deleteChart(chartID) {
+        await this.assertLogin();
+        const config = {method: "DELETE", url:`/api/charts/${chartID}`}
+        config['headers'] = {"Content-Type": "application/json","Authorization":`Bearer ${this.accessToken}`}
+        const response = await this.client(config);
+        return response.data;
+    }
+
+    async getChartCode(chartID) {
+        const config = {method: "GET", url:`/api/charts/${chartID}/code`}
+        if(this.accessToken) {
+            config['headers'] = {"Content-Type": "application/json","Authorization":`Bearer ${this.accessToken}`}
+        }
+        const response = await this.client(config);
+        return response.data;
+    }
+
+    async getChartConfig(chartID) {
+        const config = {method: "GET", url:`/api/charts/${chartID}/code`}
+        if(this.accessToken) {
+            config['headers'] = {"Content-Type": "application/json","Authorization":`Bearer ${this.accessToken}`}
+        }
+        const response = await this.client(config);
+        return response.data;
+    }
+
+    async getChartFile(chartID, filename) {
+        const config = {method: "GET", url:`/api/charts/${chartID}/${filename}`}
+        if(this.accessToken) {
+            config['headers'] = {"Content-Type": "application/json","Authorization":`Bearer ${this.accessToken}`}
+        }
+        const response = await this.client(config);
+        return response.data;
+    }
+
+    async getShares(type, id) {
+        const config = {method: "GET", url:`/api/${type}/${id}/shared`}
+        if(this.accessToken) {
+            config['headers'] = {"Content-Type": "application/json","Authorization":`Bearer ${this.accessToken}`}
+        }
+        const response = await this.client(config);
+        return response.data;
+    }
+
+    getChartShares(chartID) {
+        return this.getShares('charts',chartID);
+    }
+
+    getDatasourceShares(datasourceID) {
+        return this.getShares('datasources',datasourceID);
+    }
+
+    async modifyShare(type,method, id, users, groups) {
+        await this.assertLogin();
+        const data = {
+            users: users,
+            groups: groups,
+        };
+        const config = {method: method, url:`/api/${type}/${id}/shared`, data:data};
+        if(this.accessToken) {
+            config['headers'] = {"Content-Type": "application/json","Authorization":`Bearer ${this.accessToken}`};
+        }
+        const response = await this.client(config);
+        return response.data;
+    }
+
+    removeChartShare(id, users, groups) {
+        return this.modifyShare('charts','DELETE', id, users, groups);
+    }
+
+    addChartShare(id, users, groups) {
+        return this.modifyShare('charts','PATCH', id, users, groups);
+    }
+
+    removeDatasourceShare(id, users, groups) {
+        return this.modifyShare('datasources','DELETE', id, users, groups);
+    }
+
+    addDatasourceShare(id, users, groups) {
+        return this.modifyShare('datasources','PATCH', id, users, groups);
+    }
+
+    refreshAccessToken() {
+        if(!this.refreshToken) {
+            throw "Not logged in!";
+        }
+
+        const payload = parseJwt(this.refreshToken);
+        if(Date.now()/1000 > payload['exp']) {
+            this.logout();
+            throw "Refresh token expired";
+        }
+
+        const client = this.client;
+        const data = {refresh: this.refreshToken}
+        return this.client({method: "POST", url:`/api/token/refresh/`, data: data, headers:{"Content-Type": "application/json"}})
+            .then((response) => {
+                console.log(response)
+                client.accessToken = response.data['access'];
+            }).catch(console.error);
+    }
+
+    async assertLogin() {
+        //FIXME: Check if token is still valid
+        if(!(this.accessToken)) {
+            throw "Not logged in!";
+        }
+        const payload = parseJwt(this.accessToken);
+        if(Date.now()/1000 > payload['exp']) {
+            await this.refreshAccessToken();
+        }
+    }
+
+    async verifyToken(token) {
+        try {
+            await this.client({
+                method: "POST",
+                url: `/api/token/verify/`,
+                data: {token: token},
+                headers: {"Content-Type": "application/json"}
+            })
+            return true;
+        } catch {
+            return false;
+        }
+
     }
 }
 
@@ -72,6 +279,12 @@ function toBase64(file, onSuccess, onError) {
        reader.onload = onSuccess;
        reader.onerror = onError;
        reader.readAsDataURL(file);
+}
+
+function parseJwt(token) {
+  var base64Payload = token.split('.')[1];
+  var payload = Buffer.from(base64Payload, 'base64');
+  return JSON.parse(payload.toString());
 }
 
 export function getClient(name="default") {
